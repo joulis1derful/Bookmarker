3058f204ee409c5cf60494299b239a6e87cd67e5
joulis1derful <joulis1derful@gmail.com>
2017-08-22 21:43:32 +0300

    Added confirmation message to prevent from accidental deletion

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
 document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
 function saveBookmark(e){
-	var siteName = document.getElementById('siteName').value;
-	var siteUrl = document.getElementById('siteUrl').value;
+	let siteName = document.getElementById('siteName').value;
+	let siteUrl = document.getElementById('siteUrl').value;
 
 	if(!validation(siteName, siteUrl)){
 		return false;
 	}
 
-	var bookmark = {
+	let bookmark = {
 		name: siteName,
 		url: siteUrl
 	};
 
 	if(localStorage.getItem('bookmarks') === null){
-		var bookmarks = [];
+		let bookmarks = [];
 		bookmarks.push(bookmark);
 		localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
 	} else {
-		var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+		let bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
 		bookmarks.push(bookmark);
 		localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
 	}
@@ -29,8 +29,9 @@ function saveBookmark(e){
 }
 
 function deleteBookmark(url){
-	var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-	for(var i = 0; i < bookmarks.length; i++){
+	if(confirm("Do you really wish to delete this bookmark?")){
+	let bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+	for(let i = 0; i < bookmarks.length; i++){
 		if(bookmarks[i].url == url){
 			bookmarks.splice(i, 1);
 		}
@@ -38,15 +39,16 @@ function deleteBookmark(url){
 	localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
 
 	fetchBookmarks();
+	}
 }
 
 function fetchBookmarks(){
-	var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-	var bookmarksResults = document.getElementById('bookmarksResults');
+	let bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+	let bookmarksResults = document.getElementById('bookmarksResults');
 	bookmarksResults.innerHTML = '';
-	for(var i = 0; i < bookmarks.length; i++){
-		var name = bookmarks[i].name;
-		var url = bookmarks[i].url;
+	for(let i = 0; i < bookmarks.length; i++){
+		let name = bookmarks[i].name;
+		let url = bookmarks[i].url;
 		bookmarksResults.innerHTML += '<div class="well">' + 
 									  '<h3>'+ name +
 									  ' <a class="btn btn-default" target="_blank" href="'+url+'">Visit</a> ' +
@@ -62,8 +64,8 @@ function validation(siteName, siteUrl) {
 		return false;
 	}
 
-	var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
-	var regex = new RegExp(expression);
+	let expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+	let regex = new RegExp(expression);
 
 	if(!siteUrl.match(regex)){
 		alert('Please enter a valid URL');
